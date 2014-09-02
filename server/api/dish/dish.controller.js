8022ef63102da9ecd9949230604dc1138440b3f3
Fleur Dragan <fleur@obscure.org>
2014-09-02 12:17:38 -0700

    modified /api/dish handler GET to get from database and POST to save to database

--- server/api/dish/dish.controller.js
+++ server/api/dish/dish.controller.js
@@ -10,27 +10,50 @@
 'use strict';
 
 var _ = require('lodash');
+var Dishes = require('../../bookshelf/collections/dishes');
+var Dish = require('../../bookshelf/models/dish');
 
 // Get list of things
 exports.index = function(req, res) {
 
   if (req.method === 'GET') {
 
-    res.json([{
-      "id": "1",      
-      "name": "Chicken Vindaloo",       
-      "rating": 3,     
-      "restaurant_id": "1",
-    }]);
+    findDish({id: req.params.id}, function(err, dish) {
+
+      if (err) {
+
+        console.log(err);
+        res.send(400, err);
+
+      } else if (dish) {
+
+        res.send(201, dish);
+
+      } else {
+
+        res.send(404, "dish not found");
+
+      }
+    });
 
   } else if (req.method === 'POST') {
 
-    // get JSON object from request
-    // add to database
-    // return 201 on success
-    // return URL to get entity with as Location field
-    // on failure, return 400
-    res.send(501); 
+    findDish(req.body, function(err, dish) {
+
+      if (err) {
+
+        console.log(err);
+        res.send(400, err);
+
+      } else if (dish) {
+
+        res.send(302, "dish already exists");
+
+      } else {
+        
+        saveDish(req, res, req.body);
+      }
+    });
 
   } else {
 
@@ -38,3 +61,38 @@ exports.index = function(req, res) {
     res.send(501); 
   }
 };
+
+var saveDish = function(req, res, data) {
+
+  var dish = new Dish(data);
+  dish.save()
+  .then(function(found) {
+
+    // not sure this is supposed to be here
+    Dishes.add(found);
+    res.send(201, found);
+  })
+  .otherwise(function(err) {
+    
+    console.log(err);
+    res.send(400);
+  });
+};
+
+var findDish = function(data, callback) {
+
+  new Dish(data)
+  .fetch()
+  .then(function(found) {
+
+    Dishes.add(found);
+    callback(null, found);
+
+  })
+  .otherwise(function(err) {
+
+    callback(err, null);
+
+  });
+};
+
