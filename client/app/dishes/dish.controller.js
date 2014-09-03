7fde2c908506e1033a5036ca19cae220fd9f37b9
Stacy Hseu <stacy.hseu@gmail.com>
2014-09-02 19:25:00 -0700

    Changed dishes directory name to dish, so git rm'ed client/app/dishes/*

--- client/app/dishes/dish.controller.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('craveApp')
-  .controller('DishCtrl', ['$scope', 'DishFactory', function ($scope, DishFactory) {
-  }]);
--- client/app/dishes/dish.factory.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('craveApp')
-  .factory('DishFactory', function ($http) {
-
-  });
