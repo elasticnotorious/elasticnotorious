279051038bc0ba717967aaf26c61c63f1efb5e95
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-09-04 13:30:43 -0600

    angular api connection

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -2,4 +2,29 @@
 
 angular.module('craveApp')
   .controller('SearchCtrl', ['$scope', 'SearchFactory', function ($scope, SearchFactory) {
+    $scope.test = 'Hello World!';
+    // $scope.data = {results: [
+    //   {dish: 'TEST'},
+    //   {dish: 'TEST2'}
+    //   ]};
+    $scope.data = [];
+    $scope.requestFunction = function(){
+      //$route.reload();
+      console.log($scope.data);
+      SearchFactory.getFunction($scope.data).(function(data){
+        $scope.data = data;
+        console.log('temp-data', data);
+      });
+      console.log(temp);
+      return temp;
+      //refresh();
+    }
+
+    $scope.$watch('data', function() {
+      $scope.data = $scope.data;
+      //$scope.data
+    });
+    var refresh = function(){
+      $scope.$apply();
+    }
   }]);
--- client/app/search/search.factory.js
+++ client/app/search/search.factory.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('craveApp')
-  .factory('SearchFactory', function ($http) {
+.factory('SearchFactory', function ($http) {
 
-  });
+  var getFunction = function(callback){
+    $http({
+      method: 'GET', 
+      url: 'https://api.parse.com/1/classes/dish',
+      headers: {
+        'X-Parse-REST-API-Key': '2My02jc6sYRjSBHA8xiKxM3fjL5htIo8D7E8hgKK',
+        'X-Parse-Application-Id': 'BwN0ekIDkYFRTqDlQLEuGT5Dra19III55M4FdYzq'
+      }
+    }).success(function(data, status){
+      console.log('data', data, 'status', status);
+      console.log(data.results[0].dish);
+      callback = data.results;
+      return callback;
+    }).error(function(data, status){
+      console.log('Error', data, 'Status', status);
+    });
+  }
+
+  return {
+    getFunction: getFunction
+  };
+
+});
