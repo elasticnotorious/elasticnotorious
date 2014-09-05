637e69664b3d9a71a376aba69a69de9a8db6f478
Stacy Hseu <stacy.hseu@gmail.com>
2014-09-04 17:19:13 -0700

    Search page refreshes with items when search button is clicked.

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -8,23 +8,19 @@ angular.module('craveApp')
     //   {dish: 'TEST2'}
     //   ]};
     $scope.data = [];
-    $scope.requestFunction = function(){
-      //$route.reload();
-      console.log($scope.data);
-      SearchFactory.getFunction($scope.data).(function(data){
-        $scope.data = data;
-        console.log('temp-data', data);
-      });
-      console.log(temp);
-      return temp;
-      //refresh();
+    $scope.requestFunction = function() {
+      console.log(SearchFactory);
     }
-
-    $scope.$watch('data', function() {
-      $scope.data = $scope.data;
-      //$scope.data
-    });
-    var refresh = function(){
-      $scope.$apply();
-    }
+    var promise = SearchFactory.getFunction();
+    promise.then(function(data){
+      console.log(data);
+      $scope.data = data;
+    })
+    // $scope.$watch('data', function() {
+    //   $scope.data = $scope.data;
+    //   //$scope.data
+    // });
+    // var refresh = function(){
+    //   $scope.$apply();
+    // }
   }]);
