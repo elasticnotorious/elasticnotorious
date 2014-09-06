81a62b3e66efd65f3e9db0561361445e368425d0
Fleur Dragan <fleur@obscure.org>
2014-09-06 11:27:48 -0700

    create dish layout and form validation.  still need restaurant api fixed.

--- client/app/createdish/createdish.controller.js
+++ client/app/createdish/createdish.controller.js
@@ -3,3 +3,35 @@
 angular.module('craveApp')
   .controller('CreateDishCtrl', ['$scope', 'CreateDishFactory', function ($scope, CreateDishFactory) {
   }]);
+
+  angular.module('craveApp')
+    .controller('ExampleController', ['$scope', '$http', '$window', 'CreateDishFactory', function($scope, $http, $window, CreateDishFactory) {
+      $scope.master = {};
+      $scope.submitted = false;
+      $scope.submitting = false;
+
+      $scope.update = function(fields) {
+
+      	console.log("window: " , $window);
+
+
+        $scope.master = angular.copy(fields);
+        $scope.submitted = true;
+
+        if ($scope.createDishForm.$valid) {
+        	CreateDishFactory.save(fields);
+        }else {
+        	$window.alert('Please fix any validation errors and try again.');
+    		}
+      };
+
+      $scope.reset = function() {
+        $scope.data = angular.copy($scope.master);
+      };
+
+      $scope.getResturant = function(name) {
+      	console.log(name);
+      };
+
+      $scope.reset();
+    }]);
